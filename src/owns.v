c57f69c8aeee10febe6e6adff76bfb0584db5fdc
Evan Anchante <frothycurve@gmail.com>
2026-06-28 20:38:50 +1000

    replaced / with system_root() in owns.v

--- src/owns.v
+++ src/owns.v
@@ -1,11 +1,12 @@
 module main
 
 import store
+import fs
 
 fn cmd_owns(path string) ! {
 	mut db := store.open()!
 
-	owner := db.owner(path.trim_left('/'))!
+	owner := db.owner(path.trim_left(fs.system_root()))!
 
 	if owner == '' {
 		println('untracked (possibly system file)')
