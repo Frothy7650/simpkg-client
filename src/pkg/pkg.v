b10d80b50652920bec3861a0a9303b932d4a5204
Evan Anchante <frothycurve@gmail.com>
2026-06-29 15:37:34 +1000

    switched to TOML PKGINFO files

--- src/pkg/pkg.v
+++ src/pkg/pkg.v
@@ -1,21 +1,16 @@
 module pkg
 
 import compress.szip
+import toml
 import os
-
-pub enum PkgType {
-	binary
-	source
-	library
-}
 
 pub struct PkgInfo {
 pub mut:
 	name         string
 	version      string
-	type_        PkgType
 	deps         []string
 	files        []string
+  build        []string
 	preinstalls  []string
 	postinstalls []string
 	preremoves   []string
@@ -46,67 +41,17 @@ pub fn parse(root string) !PkgInfo {
 		return error('missing PKGINFO: ${pkginfo_path}')
 	}
 
-	lines := os.read_file(pkginfo_path)!.split_into_lines()
+	file := os.read_file(pkginfo_path)!
 
-	mut pkg := PkgInfo{
-		root: root
-	}
-
-	for line in lines {
-		if line.trim_space() == '' || line.starts_with('#') {
-			continue
-		}
-
-		parts := line.split_nth('=', 2)
-		if parts.len != 2 {
-			return error('invalid PKGINFO line: ${line}')
-		}
-
-		key := parts[0]
-		val := parts[1]
-
-		match key {
-			'name' {
-				pkg.name = val
-			}
-			'version' {
-				pkg.version = val
-			}
-			'type' {
-				match val {
-					'binary' { pkg.type_ = .binary }
-					'source' { pkg.type_ = .source }
-					'library' { pkg.type_ = .library }
-					else { return error('invalid type: ${val}') }
-				}
-			}
-			'depends' {
-				pkg.deps << val
-			}
-			'files' {
-				pkg.files << val
-			}
-			'preinstall' {
-				pkg.preinstalls << val
-			}
-			'postinstall' {
-				pkg.postinstalls << val
-			}
-			'preremove' {
-				pkg.preremoves << val
-			}
-			'postremove' {
-				pkg.postremoves << val
-			}
-			else {
-				return error('unknown key: ${key}')
-			}
-		}
-	}
+	mut pkg := toml.decode[PkgInfo](file)!
+  pkg.root = root
 
 	if pkg.name == '' {
 		return error('missing package name')
 	}
+  if pkg.files.len == 0 {
+    return error('missing package file list')
+  }
 
 	return pkg
 }
