6e9939cb624abdf634e07044ad6c6bfb8540e6ee
Evan Anchante <frothycurve@gmail.com>
2026-06-30 00:10:34 +1000

    updated dependency checks

---
+++ src/fs/deps.c.v
@@ -0,0 +1,8 @@
+module fs
+
+#flag linux -ldl
+fn C.dlopen(path &char, flag int) voidptr
+fn C.dlclose(handle voidptr) int
+#flag windows -lkernel32
+fn C.LoadLibraryA(path &char) voidptr
+fn C.FreeLibrary(hModule voidptr) bool
