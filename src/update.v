62fd589ab09561236a292d8bc2b2ece2aa4bf162
Evan Anchante <frothycurve@gmail.com>
2026-07-18 23:04:11 +1000

    Made update not use local or remote db, so it can fix broken remote dbs

--- src/update.v
+++ src/update.v
@@ -1,8 +1,17 @@
 module main
 
+import frothy7650.dag
+import net.http
 import store
+import os
 
 fn cmd_update() ! {
-	mut db := store.open()!
-	db.update_dag(config.dag_url)!
+  println('fetching ${config.dag_url}')
+  body := http.get(config.dag_url)!.body
+
+  mut g := dag.new_graph()
+  g.from_json(body)!
+
+  os.write_file(store.remote_path, body)!
+  println('done')
 }
